3afbec55d9476eae889ad5e9e71c63f620de4af8
ashu93102 <ashusharma93102@gmail.com>
2024-02-13 23:16:37 +0530

    now password will save in a txt file

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 import tkinter
+from tkinter import messagebox
+
+
+# To show popup we can use popup module
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+class Data:
+    def __init__(self, **kwargs):
+        self.website_data = kwargs.get("website_data")
+        self.email = kwargs.get("email")
+        self.passw = kwargs.get("passw")
+
+
+def userdata():
+    """Gets data from tkinter entry widget and puts it into a text file"""
+    website_data = website.get()
+    email_user_name = email.get()
+    site_password = passwd.get()
+
+    # To show Popup message box, and it returns value as Boolean data type (true or false)
+    if len(website_data) > 0 and len(site_password) > 0:
+        is_ok = messagebox.askokcancel(title=website_data,
+                                       message=f"email/username:-{email_user_name}\nPassword:-{site_password}\n"
+                                               f" Do you want to save?")
+        if is_ok:
+            with open("userdata.txt", "a") as file:
+                file.write(
+                    "website:- " + website_data + "/ " + "email/username:-" + email_user_name + "/ " + "Password:-"
+                    + site_password + "\n")
+    else:
+        messagebox.showwarning(title="Data Empty", message="Please fill blank fields")
+
+    website.delete(0, tkinter.END)
+    passwd.delete(0, tkinter.END)
+    # below line shows python class and put data into class
+    user_input_data = Data(website_data=website_data, email=email_user_name, passw=site_password)
+    print(user_input_data.website_data)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
@@ -33,15 +69,14 @@ email = tkinter.Entry(width=48)
 email.grid(column=1, row=2, columnspan=2)
 email.insert(tkinter.END, "ashu@gmail.com")
 # Entry
-passwd = tkinter.Entry(width=31)
+passwd = tkinter.Entry(width=31, show="*")
 passwd.grid(row=3, column=1)
 
 # Password generator Button
 passwd_button = tkinter.Button(text="Generate Password", highlightthickness=0, font=("Arial", 8))
 passwd_button.grid(column=2, row=3)
 # Add button
-add_button = tkinter.Button(text="Add", width=40)
+add_button = tkinter.Button(text="Add", width=40, command=userdata)
 add_button.grid(column=1, row=4, columnspan=2)
-
 
 window.mainloop()
